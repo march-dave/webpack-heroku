948b232d211487b8c2ff0315ae9988d2d83f7331
march-dave <ysangy@gmail.com>
2016-06-12 16:25:03 -0700

    use import

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const Server = require('./server.js')
+import Server from './server.js'
 const port = (process.env.PORT || 8080)
 const app = Server.app()
 
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.dev.config.js')
+  import webpack = from 'webpack'
+  import webpackDevMiddleware = from 'webpack-dev-middleware'
+  import webpackHotMiddleware = from 'webpack-hot-middleware'
+  import config = from './webpack.dev.config.js'
   const compiler = webpack(config)
 
   app.use(webpackHotMiddleware(compiler))
