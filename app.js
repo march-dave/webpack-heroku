3cde6bdbb9f4340b0405d94b313948c6207f740a
march-dave <ysangy@gmail.com>
2016-06-12 23:04:51 -0700

    app.js update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import Server from './server.js'
+import webpack from 'webpack';
 const port = (process.env.PORT || 8080)
 const app = Server.app()
 
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.dev.config.js')
