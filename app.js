f440220b575fdde83dda3677f2776b2028a4635e
march-dave <ysangy@gmail.com>
2016-06-12 23:31:14 -0700

    add config

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ import cookieParser from 'cookie-parser';
 import graphqlHTTP from 'express-graphql'
 // import schema from './data/index'
 
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/property-manager');
+const port = (process.env.PORT || 8080);
 
-
-
-
-const port = (process.env.PORT || 8080)
 const app = Server.app()
 
 if (process.env.NODE_ENV !== 'production') {
@@ -30,6 +29,23 @@ if (process.env.NODE_ENV !== 'production') {
     publicPath: config.output.publicPath
   }))
 }
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', api)
+
+app.use('/graphql', graphqlHTTP ({
+  schema:schema,
+  pretty: true,
+  graphql: true
+}))
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
 
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
