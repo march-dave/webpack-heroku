37397bb7ff3c2dc49b44a4dbcf05bfa16485a805
march-dave <ysangy@gmail.com>
2016-06-12 22:31:13 -0700

    update webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,12 @@
-const path = require('path')
-const webpack = require('webpack')
+import webpack from 'webpack'
+import path from 'path'
 
 module.exports = {
-  devtool: 'eval',
-
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
+    'webpack-hot-middleware/client?reload=true',
+    'bootstrap-loader',
     './src/index'
   ],
 
@@ -14,7 +15,9 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/public/'
   },
-
+  devServer: {
+    contentBase: './src'
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
@@ -32,7 +35,15 @@ module.exports = {
       { test: /\.png$/,
         loader: 'file' },
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
+        loader: 'file'},
+        {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      // Bootstrap 3 jquery requirement
+      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
+
     ]
   }
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,19 +1,22 @@
-const path = require('path')
-const webpack = require('webpack')
+import webpack from 'webpack'
+import path from 'path'
 
 module.exports = {
   devtool: 'source-map',
 
   entry: [
+    'bootstrap-loader',,
     './src/index'
   ],
-
+  target: 'web',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
-
+  devServer: {
+    contentBase: './src'
+  },
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
@@ -40,7 +43,14 @@ module.exports = {
       { test: /\.png$/,
         loader: 'file' },
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
+        loader: 'file'},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      // Bootstrap 3 jquery requirement
+      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
     ]
   }
 }
