6bf0908b13058481c8fdd5f368f9b30049cfa637
march-dave <ysangy@gmail.com>
2016-06-20 11:11:28 -0700

    NavBar add

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello World! and React JS</h1>
+        <NavBar />
+        <h1>Hello World! and React JS 1</h1>
       </div>
     )
   }
