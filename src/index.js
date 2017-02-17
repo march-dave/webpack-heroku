8ee9979f11df1759e38f37ac9cf2dfb69ff627d2
march-dave <ysangy@gmail.com>
2017-02-16 19:48:02 -0500

    scss path error fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom'
 import { Router, Route, Link, browerHistory, IndexRoute } from 'react-router'
 
 import App from './components/app'
-import app from '../styles/app.scss'
+// import '../styles/app.scss'
 
-render(<App/>, document.getElementById('main'))
+render( < App / > , document.getElementById('main'))
